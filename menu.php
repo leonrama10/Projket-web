993c86ca78718631071a85fd1618c8f828ac5717
Your Name <denisdushi19@gmail.com>
2024-01-31 13:47:08 +0100

    Kthimi nga html ne php

---
+++ menu.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+
+    <link rel="stylesheet" href="menu.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <div class="site-logo">
+                <h1 class="s-l-h1-1">L&D MEAT</h1>
+            </div>
+            <nav class="site-nav">
+                <ul class="site-nav_menu">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="menu.html">MENUS</a></li>
+                    <li><a href="PROJEKTI.html">LOGIN</a></li>
+                    <li><a href="About-us.html">ABOUT US</a></li>
+                    <li><a href="#">LOCATIONS</a></li>
+                    <li><a href="#">BBQ BOX</a></li>
+                    <li><a class="border-a-1" href="#">RESERVING</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div class="open-d">
+        <div class="od-text">
+            <h1 class="od-t-h1-1">MENUS</h1>
+            <p class="od-t-p-1">A culinary tour of BBQ traditions from around this great nation and this <br> 
+                beautiful planet. 1.8 million years ago, humans first began cooking meat with <br> 
+                fire. For some reason, this hasn’t led to world peace, but we’re pretty sure if we <br>
+                 fire up enough BBQ, it will.
+            </p>
+        </div>
+    </div>
+    
+    <div class="tabs">
+        <div class="tabs-nav">
+            <ul class="tn-ul">
+                <li><a class="tn-ul-a" href="#">Main menu</a></li>
+                <li><a class="tn-ul-a" href="#">lunch happy hour</a></li>
+                <li><a class="tn-ul-a" href="#">specials</a></li>
+                <li><a class="tn-ul-a" href="#">catering</a></li>
+            </ul>
+            <p class="tn-p-1">
+                Indoor and outdoor dining now open! Check out our menu on Toast and follow <br> 
+                us on social media to hear about weekly specials and rotating "faux que" <br> 
+                vegetarian offerings.
+            </p>
+            <a class="tn-a-1" href="#">order now</a>
+        </div>
+
+        <div class="grid-full">
+            <div class="gf-left">
+                <h2 class="gf-h2-1">barbeque</h2>
+                <p class="gf-p-1">Smoked daily, all served with Texas toast & pickles. Sold by the <br> 
+                    1/2 lb.
+                </p>
+
+                <div class="gf-img-1">
+                    <img class="gf-foto-1" src="images/menu-foto-1.avif" alt="logo">
+                    <p class="gf-img-p-1">Brisket</p>
+                    <p class="gf-img-p-2">chopped or sliced</p>
+                    <ul class="gf-img-ul-1">
+                        <li>PORK RIB TIPS</li>
+                        <li>PORK SPARE RIBS</li>
+                        <li>PULLED PORK SHOULDER</li>
+                        <li>PORK BELLY</li>
+                        <li>TURKEY BREAST</li>
+                        <li>CHICKEN</li>
+                    </ul>
+
+                    <img class="gf-foto-2" src="images/menu-foto-2.avif" alt="logo">
+                    <ul class="gf-img-ul-2">
+                        <li class="gf-li-2">CHICKEN WINGS</li>
+                        <li class="gf-li-2">JALAPEÑO CHEESE SAUSAGE</li>
+                    </ul>
+                    <hr class="hr-1">
+
+                    <h2 class="gf-h2-2">PLATTERS AND COMBOS</h2>
+                    <p class="gf-img-p-3">Creating the *perfect* palette of Federalist Pig flavors is an art. <br> 
+                        And we’re artists. Served with Texas toast & pickles.
+                    </p>
+
+                    <ul class="title-img-ul">
+                        <li class="tiu-li-1">two step</li>
+                        <li class="tiu-li-2">pick any 2 meats & 2 small sides</li>
+                        <li class="tiu-li-1">RIB & CHICKEN</li>
+                        <li class="tiu-li-2">1/2 lb. of ribs, 1/4 chicken & 2 small sides</li>
+                        <li class="tiu-li-1">SAMPLER PLATTER</li>
+                        <li class="tiu-li-2">pick any 3 meats & 2 small sides</li>
+                        <li class="tiu-li-1">PITMASTER SPECIAL FOR 2</li>
+                        <li class="tiu-li-2">pick any 4 meats & 3 small sides</li>
+                    </ul>
+
+                    <img class="gf-foto-3" src="images/menu-foto-3.avif" alt="logo">
+                    <p class="gf-img-p-4">FEED THE FAM</p>
+                    <p class="gf-img-p-5">pick any 4 meats & 4 large sides</p>
+                    <hr class="hr-2">
+
+                    <ul class="last-for-ul">
+                        <li class="lfu-li-1">SIDES</li>
+                        <li class="lfu-li-2">A wise leader once said that unless both sides win, no agreement <br> 
+                            can be permanent. So — just agree to get more sides later.
+                        </li>
+                        <li class="lfu-li-1">SMOKED CHEDDAR MAC & CHEESE</li>
+                        <li class="lfu-li-2">shell pasta baked with smoked cheddar and jack cheese</li>
+                        <li class="lfu-li-1">SEASONED FRENCH FRIES</li>
+                        <li class="lfu-li-2">skin-on fries seasoned with Fed Pig BBQ rub</li>
+                        <li class="lfu-li-1">CRISPY BRUSSELS SPROUTS</li>
+                        <li class="lfu-li-2">fried and seasoned with Fed Pig BBQ rub</li>
+                        <li class="lfu-li-1">CHIPOTLE GARLIC GREEN BEANS</li>
+                        <li class="lfu-li-2">stewed with roasted garlic and chipotle peppers</li>
+                        <li class="lfu-li-1">RED BLISS POTATO SALAD</li>
+                        <li class="lfu-li-2">southern style potato salad with chopped pickles, celery, and <br> 
+                            hard boiled eggs
+                        </li>
+                        <li class="lfu-li-1">CLASSIC COLESLAW</li>
+                        <li class="lfu-li-2">carolina style shredded cabbage slaw</li>
+                        <li class="lfu-li-1">BBQ BEANS</li>
+                        <li class="lfu-li-2">smoky, savory, and a little sweet</li>
+                        <li class="lfu-li-1">LOADED FRIES</li>
+                        <li class="lfu-li-2">seasoned fries topped with pulled pork, melted cheese, ranch, <br> 
+                            and BBQ sauce
+                        </li>
+                    </ul>
+
+                </div>
+                
+              </div>
+            <div class="gf-right">
+                <div class="down-right">
+                    <h2 class="gr-h2-1">SIGNATURE <br> SANDWICHES</h2>
+                    <ul class="gfr-ul-1">
+                        <li class="gfr-li-2">When bread meets what could go between bread, there’s no end <br> 
+                            to the culinary possibilities.
+                        </li>
+                        <li class="gfr-li-1">CAROLINA ON MY MIND</li>
+                        <li class="gfr-li-2">pulled pork, crispy skin, coleslaw, spicy vinegar sauce, sesame <br> 
+                            bun
+                        </li>
+                        <li class="gfr-li-1">MUSIC CITY</li>
+                        <li class="gfr-li-2">spicy fried chicken breast, greens, pickles, buttermilk ranch, <br> 
+                            sesame bun
+                        </li>
+                        <li class="gfr-li-1">BIG CHEESE</li>
+                        <li class="gfr-li-2">chopped pork or brisket, melted cheese, crispy onions, BBQ <br> 
+                            sauce, sesame bun
+                        </li>
+                        <li class="gfr-li-1">CLUB</li>
+                        <li class="gfr-li-2">smoked turkey, crispy pork belly, avocado, tomato, garlic aioli, <br> 
+                            texas toast
+                        </li>
+                    </ul>
+
+                    <img class="gf-foto-4" src="images/menu-foto-4.avif" alt="logo">
+
+                    <ul class="gfr-ul-2">
+                        <li class="gfr-li-3">JIVE TURKEY</li>
+                        <li class="gfr-li-4">smoked turkey, melted cheese, grilled tomato, crispy onions, <br> 
+                            ranch, BBQ sauce, texas toast
+                        </li>
+                        <li class="gfr-li-3">TEXAS RANGER</li>
+                        <li class="gfr-li-4">sliced brisket, crispy onions, house pickles, BBQ sauce, texas <br> 
+                            toast
+                        </li>
+                        <li class="gfr-li-3">JIMMY MUSCLES</li>
+                        <li class="gfr-li-4">crispy chicken breast, pork belly, jalapeño relish, sticky garlic <br> 
+                            sauce, ranch, herbs, sesame bun
+                        </li>
+                        <li class="gfr-li-3">TEXAS FLOOD</li>
+                        <li class="gfr-li-4">chopped brisket, jalapeño cheese sausage, spicy pickles, crispy <br> 
+                            onions, bbq sauce, sesame bun
+                        </li>
+                        <li class="gfr-li-3">TOFU BANH MI</li>
+                        <li class="gfr-li-4">crispy tofu burnt ends, pickled veggies, fresh herbs, crispy <br> 
+                            onions, sticky garlic, aioli, hoagie roll
+                        </li>
+                    </ul>
+                    <hr class="hr-right-1">
+
+                    <ul class="gfr-ul-3">
+                        <li class="gfr-h2-1">TRADITIONAL SANDWICHES</li>
+                        <li class="gfr-li-6">1/3 lb. of any off-the-bone meat on a sesame bun with BBQ <br> 
+                            sauce. Ask not what BBQ can do for the sandwich — just get the <br> 
+                            sandwich. You can figure out the rest later.
+                        </li>
+                        <li class="gfr-li-5">BRISKET</li>
+                        <li class="gfr-li-6">chopped or sliced</li>
+                        <li class="gfr-li-5">SLICED SMOKED TURKEY</li>
+                        <li class="gfr-li-5">PULLED PORK</li>
+                        <li class="gfr-li-5">SLICED PORK BELLY</li>
+                    </ul>
+                    <hr class="hr-right-2">
+
+                    <ul class="gfr-ul-4">
+                        <li class="gfr-h2-2">SALADS</li>
+                        <li class="gfr-li-8">Crunch crunch, who's there? You, getting a tangy, acidic crunch <br> 
+                            of greens to really make your plate sing.
+                        </li>
+                        <li class="gfr-li-7">BBQ GREEN SALAD</li>
+                        <li class="gfr-li-8">Choice of meat, mixed greens, smoked tomato, pickles, crispy <br> 
+                            onions, apple cider vinaigrette
+                        </li>
+                    </ul>
+                    <hr class="hr-right-3">
+
+                    <ul class="gfr-ul-5">
+                        <li class="gfr-h2-3">DESSERT</li>
+                        <li class="gfr-li-10">The kind of blue-ribbon treats usually reserved for a State Fair <br> 
+                            judge, straight to you.
+                        </li>
+                        <li class="gfr-li-9">NANA'S BANANA NANA PUDDING</li>
+                        <li class="gfr-li-10">fresh bananas layered with vanilla pudding and nilla wafers</li>
+                        <li class="gfr-li-9">OREO CHOCOLATE PUDDING</li>
+                        <li class="gfr-li-10">chocolate pudding layered with oreo cookie crumbles</li>
+                    </ul>
+                    <hr class="hr-right-3">
+
+                    <ul class="gfr-ul-6">
+                        <li class="gfr-h2-4">ALLERGEN INFO</li>
+                        <li class="last-ul-4">NO NUTS OR NUT OILS USED</li>
+                        <li class="last-ul-2">Contains dairy:</li>
+                        <li class="last-ul-1">Crispy Chicken Breast</li>
+                        <li class="last-ul-1">Mac & Cheese</li>
+                        <li class="last-ul-1">Ranch Dressing</li>
+                        <li class="last-ul-1">Blue Cheese Dressing</li>
+                        <li class="last-ul-1">Crispy Onions</li>
+                        <li class="last-ul-1">Texas Toast</li>
+                        <li class="last-ul-1">Nana's Banana Nana Pudding</li>
+                        <li class="last-ul-1">Oreo Chocolate Pudding</li>
+                        <li class="last-ul-3">Contains Gluten:</li>
+                        <li class="last-ul-1">Crispy Chicken Breast</li>
+                        <li class="last-ul-1">Mac & Cheese</li>
+                        <li class="last-ul-1">Crispy Onions</li>
+                        <li class="last-ul-1">Texas Toast</li>
+                        <li class="last-ul-1">Potato Bun</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+     <div class="footer">
+      <div class="footer-links">
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        <a class="fd-1" href="https://www.ubt-uni.net/sq/ballina/">Home</a>
+        <a href="#"><i class="far fa-envelope"></i></a>
+        <a class="fd-1" href="https://www.google.com/search?q=gmail&oq=gmail+&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MhIIARAAGBQYgwEYhwIYsQMYgAQyEggCEAAYFBiDARiHAhixAxiABDINCAMQABiDARixAxiABDINCAQQABiDARixAxiABDIKCAUQABixAxiABDIGCAYQRRg8MgYIBxBFGD3SAQg5OTIwajBqNKgCALACAA&sourceid=chrome&ie=UTF-8">About</a>
+        <a href="#"><i class="fas fa-map-marker-alt"></i></a>
+        <a class="fd-1" href="https://www.google.com/maps/place/Innovation+Campus+-+UBT/@42.5582459,21.1317994,16.25z/data=!4m5!3m4!1s0x13549d2cc6e13dd5:0xf9155209d4ad0657!8m2!3d42.5581825!4d21.1347087">Services</a>
+        <a href="#"><i class="fas fa-phone"></i></a>
+        <a class="fd-1" href="https://www.ubt-uni.net/sq/ubt/kontakti//">Contact</a>
+      </div>
+  </div>
+    
+</body>
+</html>
