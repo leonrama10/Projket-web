a734593df1e76bbe15610d75cb39f0790c6b95c1
leonrama10 <151144347+leonrama10@users.noreply.github.com>
2024-03-23 16:02:41 +0100

    Ndryshime ne link

--- menu.php
+++ menu.php
@@ -22,13 +22,13 @@
             <nav class="site-nav">
                 
                 <ul class="site-nav_menu">
-                    
+                    <!-- <li><a href="Administrator.php">Admin</a></li> -->
                     <li><a href="index.php">Home</a></li>
                     <li><a href="menu.php">MENUS</a></li>
-                    <!-- <li><a href="LOGINFORM.php"></a></li> -->
+                    <li><a href="LOGINFORM.php">LOGIN</a></li>
                     <li><a href="About-us.php">ABOUT US</a></li>
                     <li><a href="#">LOCATIONS</a></li>
-                    <!-- <li><a href="RegisterForm.php">REGISTER</a></li> -->
+                    <li><a href="RegisterForm.php">REGISTER</a></li>
                     <li><a class="border-a-1" href="#">RESERVING</a></li>
                 </ul>
             </nav>
